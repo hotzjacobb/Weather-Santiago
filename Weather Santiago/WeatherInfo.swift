7f00264b15a2f025a7382b60dc366c46fb2db91e
jeffhotz <hotzjacobb@yahoo.com>
2019-10-23 20:59:54 -0700

    Condensing array variable created; onto implementation

--- Weather Santiago/WeatherInfo.swift
+++ Weather Santiago/WeatherInfo.swift
@@ -36,11 +36,15 @@ struct FiveDayData:Decodable {
 
 struct TempWrapperObj:Decodable {
     var temp: Double
+    var temp_min: Double
+    var temp_max: Double
 }
 
 func averageFiveDayData(_ forecasts: [FiveDayData]) {      // Because I'm using the free version of the API I have to extrapolate 3-hour interval forecasts into days
-    // Args: Every three hour forecast recieved from the JSON
-    var day1, day2, day3, day4, day5 : [FiveDayData]
+    // Args: Every three hour forecast rec eved from the JSON
+    //var day1 = [FiveDayData](), day2 = [FiveDayData](), day3 = [FiveDayData](), day4 = [FiveDayData](), day5 = [FiveDayData]()
+    //var arrayOfDayArrays = [day1, day2, day3, day4, day5]        // note: Swift doesn't use pointers; it hard copies
+    var arrayOfDayArrays = [[FiveDayData](), [FiveDayData](), [FiveDayData](), [FiveDayData](), [FiveDayData]()]
     // first get into appropriate groupings
     // start by getting the first day
     guard var dayStart: String.Index = forecasts[0].dt_txt.lastIndex(of: "-") else {     // last index is a way to hack Swift's non-intuitive methods for index of
@@ -52,10 +56,27 @@ func averageFiveDayData(_ forecasts: [FiveDayData]) {      // Because I'm using 
     // Swift methods to add or subtract from a string index; adjusting to get the right substring
     // note that the API uses YY-MM-DD
     dayStart = forecasts[0].dt_txt.index(dayStart, offsetBy: 1)
-    var firstDay = forecasts[0].dt_txt[dayStart..<dayEnd]
-    for i in 0..<forecasts.count {
-        while (firstDay == firstDay) {
+    // Start to perform condensing operation; We have to initialize a lot of variable to later compute avg. temp and high and low.
+    var condensedFinalArray: [FiveDayData]        // array with five elements; one for each day
+    var dayIndex = 0;                             // index in the final array of the entries we a currently processing; increment once we see new day
+    var currentDay = forecasts[0].dt_txt[dayStart..<dayEnd]       // The Swift substring method
+    var arrayElementDay: Substring
+    var forecast = 0                       // the index of the forecast in the original array
+    var numberOfEntriesDaily: Int = 0          // number of forecasts for the day; used to calc. avg. temp.
+    var lowArrayDaily: [Double]
+    var HighArrayDaily: [Double]
+    while (forecast < forecasts.count) {  // check to see if the next entry is the same day as the previous entry or a new day
+        arrayElementDay = forecasts[forecast].dt_txt[dayStart..<dayEnd]
+        if (arrayElementDay == currentDay) {        // still on the same day; add to the dayx array and continue
             
+        } else {                                    // new day; calculate old day and then add to dayx for this day
+            var medianForecastIndex: Int = arrayOfDayArrays[dayIndex].count / 2                                   // (floor(array.count/2) is the index) forecast
+            let medianForecast: WeatherData = arrayOfDayArrays[dayIndex][medianForecastIndex].weather[0]
+            var calculatedWeatherData = WeatherData(id: medianForecast.id, main: medianForecast.main, description: medianForecast.description, icon: medianForecast.icon)      // note: to get the weather description we take the values from the "median"
+            //var calculatedDay = WeatherDataTemp()
+            condensedFinalArray.append(calculatedDay)
+            dayIndex += 1
         }
+        forecast += 1
     }
 }
